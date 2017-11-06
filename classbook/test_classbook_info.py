1409b9ba47526a4482ceb17536e758dd06171a5b
I0xdI <sergo.moreno@gmail.com>
2017-11-06 18:49:08 +0700

    Beautify JSON and use YAPF formating

--- classbook/test_classbook_info.py
+++ classbook/test_classbook_info.py
@@ -4,128 +4,208 @@ import lib
 db = lib.db()
 ws = lib.ws()
 
+
 def setup():
-	with db.cursor() as cur:
-		cur.execute("""DELETE FROM classbook""")
-		cur.execute("""DELETE FROM classbook_localization""")
-		cur.execute("""INSERT INTO classbook VALUES(
-			 202, 0, 6, '098', '1f43f', 'test', 
-			 'test', '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
-		cur.execute("""INSERT INTO classbook VALUES(
-			 203, 0, 7, '098', '1f43f', 'test_with_lang', 
-			 'test', '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
-		cur.execute("""INSERT INTO classbook_localization VALUES(
-			 204, 203, 'ru', 'тест локализации', 'локализация',
-			 '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
+    with db.cursor() as cur:
+        cur.execute("""DELETE FROM classbook""")
+        cur.execute("""DELETE FROM classbook_localization""")
+        cur.execute("""INSERT INTO classbook VALUES(
+             202, 0, 6, "098", "1f43f", "test", 
+             "test", "2017-10-10 10:10:10", "2017-10-10 10:10:10")""")
+        cur.execute("""INSERT INTO classbook VALUES(
+             203, 0, 7, "098", "1f43f", "test_with_lang", 
+             "test", "2017-10-10 10:10:10", "2017-10-10 10:10:10")""")
+        cur.execute("""INSERT INTO classbook_localization VALUES(
+             204, 203, "ru", "тест локализации", "локализация",
+             "2017-10-10 10:10:10", "2017-10-10 10:10:10")""")
+
 
 def teardown():
-	with db.cursor() as cur:
-		cur.execute("""DELETE FROM classbook""")
-		cur.execute("""DELETE FROM classbook_localization""")
+    with db.cursor() as cur:
+        cur.execute("""DELETE FROM classbook""")
+        cur.execute("""DELETE FROM classbook_localization""")
+
 
 def test_info_non_found():
-	"""Тестирует classbook_infо на несуществующую статью"""
-	json_request = json.dumps(
-		{"classbookid":23232,"cmd":"classbook_info","m":"m8431"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{"cmd":"classbook_info",
-		"code": 404,
-		"error": "Not found the article",
-		"m":"m8431","result":"FAIL"}""")
-	assert response == must_be
+    """Тестирует classbook_infо на несуществующую статью"""
+    json_request = json.dumps({
+        "classbookid": 23232,
+        "cmd": "classbook_info",
+        "m": "m8431"
+    })
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads("""{
+                    "cmd": "classbook_info",
+                    "code": 404,
+                    "error": "Not found the article",
+                    "m": "m8431",
+                    "result": "FAIL"
+                    }""")
+    assert response == must_be
+
 
 def test_info_without_lang():
-	"""Testcase for handler classbook_info without lang param"""
-	json_request = json.dumps(
-		{"classbookid":202,"cmd":"classbook_info","m":"m843"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{"cmd":"classbook_info",
-		"data":
-		{"classbookid":202,"content":"test","lang":"en",
-		"langs":{},"name":"test","parentid":0,"uuid":"098"},
-		"m":"m843","result":"DONE"}""")
-	assert response == must_be
+    """Testcase for handler classbook_info without lang param"""
+    json_request = json.dumps({
+        "classbookid": 202,
+        "cmd": "classbook_info",
+        "m": "m843"
+    })
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads("""{
+                "cmd": "classbook_info",
+                "data": {
+                    "classbookid": 202,
+                    "content": "test",
+                    "lang": "en",
+                    "langs": {},
+                    "name": "test",
+                    "parentid": 0,
+                    "parents": [
+                        {
+                            "classbookid": 0,
+                            "name": "Root",
+                            "parentid": 0
+                        }
+                    ],
+                    "uuid": "098"
+                },
+                "m": "m843",
+                "result": "DONE"
+            }""")
+    assert response == must_be
+
 
 def test_info_with_lang_with_local():
-	"""Тестирует classbook_infо с указанным языком, есть локализация"""
-	json_request = json.dumps(
-		{"classbookid":203, "lang":"ru", "cmd":"classbook_info","m":"m844"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{'cmd': 'classbook_info', 'data': 
-		{'classbookid': 203, 'content': 'локализация', 'lang': 'ru', 
-		'langs':
-		 {'ru': 204}, 
-		 'name': 'тест локализации', 'parentid': 0, 'uuid': '098'}, 
-		'm': 'm844', 'result': 'DONE'}""".replace("'", "\""))
-	assert response == must_be
+    """Тестирует classbook_infо с указанным языком, есть локализация"""
+    json_request = json.dumps({
+        "classbookid": 203,
+        "lang": "ru",
+        "cmd": "classbook_info",
+        "m": "m844"
+    })
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads("""{
+                    "cmd": "classbook_info",
+                    "data": {
+                        "classbookid": 203,
+                        "content": "локализация",
+                        "lang": "ru",
+                        "langs": {
+                            "ru": 204
+                        },
+                        "name": "тест локализации",
+                        "parentid": 0,
+                        "parents": [
+                            {
+                                "classbookid": 0,
+                                "name": "Root",
+                                "parentid": 0
+                            }
+                        ],
+                        "uuid": "098"
+                    },
+                    "m": "m844",
+                    "result": "DONE"
+                }""")
+    assert response == must_be
+
 
 def test_info_with_lang_with_out_local():
-	"""Тестирует classbook_infо с указанным языком, отсутствует локализация"""
-	json_request = json.dumps(
-		{"classbookid":202, "lang":"ru", "cmd":"classbook_info","m":"m82"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{"cmd":"classbook_info",
-		"data":
-		{"classbookid":202,"content":"test","lang":"en",
-		"langs":{},"name":"test","parentid":0,"uuid":"098"},
-		"m":"m82","result":"DONE"}""")
-	assert response == must_be
+    """Тестирует classbook_infо с указанным языком, отсутствует локализация"""
+    json_request = json.dumps({
+        "classbookid": 202,
+        "lang": "ru",
+        "cmd": "classbook_info",
+        "m": "m82"
+    })
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads("""{
+                "cmd": "classbook_info",
+                "data": {
+                    "classbookid": 202,
+                    "content": "test",
+                    "lang": "en",
+                    "langs": {},
+                    "name": "test",
+                    "parentid": 0,
+                    "uuid": "098",
+                    "parents": [
+                        {
+                            "classbookid": 0,
+                            "name": "Root",
+                            "parentid": 0
+                        }
+                    ]
+                },
+                "m": "m82",
+                "result": "DONE"
+            }""")
+    assert response == must_be
+
 
 def test_info_with_lang_not_exists_article():
-	"""Тестирует classbook_infо с указанным языком, несуществующей статьей"""
-	json_request = json.dumps(
-		{"classbookid":2022, "lang":"ru", "cmd":"classbook_info","m":"m82"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{'cmd': 'classbook_info', 'code': 404, 
-		'error': 'Not found the article', 
-		'm': 'm82', 'result': 'FAIL'}""".replace("'", "\""))
-	assert response == must_be
+    """Тестирует classbook_infо с указанным языком, несуществующей статьей"""
+    json_request = json.dumps({
+        "classbookid": 2022,
+        "lang": "ru",
+        "cmd": "classbook_info",
+        "m": "m82"
+    })
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads("""{"cmd": "classbook_info", "code": 404, 
+        "error": "Not found the article", 
+        "m": "m82", "result": "FAIL"}""")
+    assert response == must_be
+
 
 def test_info_with_unsupported_lang():
-	"""Тестирует classbook_infо с неподдерживаемым языком"""
-	json_request = json.dumps(
-		{"classbookid":202, "lang":"er", "cmd":"classbook_info","m":"m82"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{
-		    "cmd": "classbook_info",
-		    "code": 404,
-		    "error": "Language is'not support",
-		    "m": "m82",
-		    "result": "FAIL"}""")
-	assert response == must_be
+    """Тестирует classbook_infо с неподдерживаемым языком"""
+    json_request = json.dumps({
+        "classbookid": 202,
+        "lang": "er",
+        "cmd": "classbook_info",
+        "m": "m82"
+    })
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads("""{
+            "cmd": "classbook_info",
+            "code": 404,
+            "error": "Language is'not support",
+            "m": "m82",
+            "result": "FAIL"}""")
+    assert response == must_be
+
 
 def test_info_with_unsupported_lang_not_exists_article():
-	"""Тестирует classbook_infо с неподдерживаемым языком
-	несуществующей статьи"""
-	json_request = json.dumps(
-		{"classbookid":20222, "lang":"er", "cmd":"classbook_info","m":"m82"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{
-		    'cmd': 'classbook_info',
-		    'code': 404,
-		    'error': 'Not found the article',
-		    'm': 'm82',
-		    'result': 'FAIL'
-			}""".replace("'", "\""))
-	assert response == must_be
+    """Тестирует classbook_infо с неподдерживаемым языком
+    несуществующей статьи"""
+    json_request = json.dumps({
+        "classbookid": 20222,
+        "lang": "er",
+        "cmd": "classbook_info",
+        "m": "m82"
+    })
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads("""{
+            "cmd": "classbook_info",
+            "code": 404,
+            "error": "Not found the article",
+            "m": "m82",
+            "result": "FAIL"
+            }""")
+    assert response == must_be
--- classbook/test_classbook_list.py
+++ classbook/test_classbook_list.py
@@ -1,78 +1,86 @@
-import json, lib
+import json
+import lib
 
 db = lib.db()
 ws = lib.ws()
 
+
 def setup():
-	with db.cursor() as cur:
-		cur.execute("""DELETE FROM classbook""")
-		cur.execute("""DELETE FROM classbook_proposal""")
-		cur.execute("""DELETE FROM classbook_localization""")
-		cur.execute("""INSERT INTO classbook VALUES(
+    with db.cursor() as cur:
+        cur.execute("""DELETE FROM classbook""")
+        cur.execute("""DELETE FROM classbook_proposal""")
+        cur.execute("""DELETE FROM classbook_localization""")
+        cur.execute("""INSERT INTO classbook VALUES(
 			 100, 0, 1, '098', '1f43f', 'test1', 'test1', 
 			 '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
-		cur.execute("""INSERT INTO classbook VALUES(
+        cur.execute("""INSERT INTO classbook VALUES(
 			 102, 0, 2, '098', '1f43f', 'test2', 'test2', 
 			 '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
-		cur.execute("""INSERT INTO classbook VALUES(
+        cur.execute("""INSERT INTO classbook VALUES(
 			 103, 0, 3, '098', '1f43f', 'test2', 'test2', 
 			 '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
-		cur.execute("""INSERT INTO classbook VALUES(
+        cur.execute("""INSERT INTO classbook VALUES(
 			 104, 100, 2, '098', '1f43f', 'test1test1', 'test1', 
 			 '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
-		cur.execute("""INSERT INTO classbook_proposal VALUES(
+        cur.execute("""INSERT INTO classbook_proposal VALUES(
 			 105, 102, 'en', 'proposal', 'test1', 
 			 '2017-10-10 10:10:10')""")
-		cur.execute("""INSERT INTO classbook_localization VALUES(
+        cur.execute("""INSERT INTO classbook_localization VALUES(
 			 104, 104, 'ru', 'тест локализации', 'локализация',
 			 '2017-10-10 10:10:10', '2017-10-10 10:10:10')""")
 
+
 def teardown():
-	with db.cursor() as cur:
-		cur.execute("""DELETE FROM classbook""")
-		cur.execute("""DELETE FROM classbook_proposal""")
-		cur.execute("""DELETE FROM classbook_localization""")
+    with db.cursor() as cur:
+        cur.execute("""DELETE FROM classbook""")
+        cur.execute("""DELETE FROM classbook_proposal""")
+        cur.execute("""DELETE FROM classbook_localization""")
 
 
 def test_with_default_parentid():
-	"""Testcase for classbook_list handler with default parentid=0""" 
-	json_request = json.dumps({"parentid":0,"cmd":"classbook_list","m":"m7334"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{"cmd":"classbook_list",
+    """Testcase for classbook_list handler with default parentid=0"""
+    json_request = json.dumps(
+        {"parentid": 0, "cmd": "classbook_list", "m": "m7334"})
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads(
+            """{"cmd":"classbook_list",
 		"data":[
 		{"childs":1,"classbookid":100,"name":"test1","parentid":0,"proposals":0},
 		{"childs":0,"classbookid":102,"name":"test2","parentid":0,"proposals":1},
 		{"childs":0,"classbookid":103,"name":"test2","parentid":0,"proposals":0}],
 		"m":"m7334","result":"DONE"}""")
-	assert response == must_be
+    assert response == must_be
+
 
 def test_with_not_exists_parentid():
-	"""Testcase for classbook_list handler with not exists parentid""" 
-	json_request = json.dumps({"parentid":432,"cmd":"classbook_list","m":"m7334"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{
+    """Testcase for classbook_list handler with not exists parentid"""
+    json_request = json.dumps(
+        {"parentid": 432, "cmd": "classbook_list", "m": "m7334"})
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads(
+            """{
 			    'cmd': 'classbook_list',
 			    'code': 404,
 			    'error': 'Not found the article with a given parentid',
 			    'm': 'm7334',
 			    'result': 'FAIL'
 			}""".replace("'", "\""))
-	assert response == must_be
+    assert response == must_be
+
 
 def test_with_not_default_parentid():
-	"""Testcase for classbook_list handler with not default parentid""" 
-	json_request = json.dumps({"parentid":100,"cmd":"classbook_list","m":"m7334"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{
+    """Testcase for classbook_list handler with not default parentid"""
+    json_request = json.dumps(
+        {"parentid": 100, "cmd": "classbook_list", "m": "m7334"})
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads(
+            """{
 			    'cmd': 'classbook_list',
 			    'data': [{
 			        'childs': 0,
@@ -84,18 +92,19 @@ def test_with_not_default_parentid():
 			    'm': 'm7334',
 			    'result': 'DONE'
 			}""".replace("'", "\""))
-	assert response == must_be
+    assert response == must_be
+
 
 def test_with_exist_lang_default_parentid():
-	"""Testcase for classbook_list handler with exist localization
-	 lang=ru and default parentid""" 
-	json_request = json.dumps({"parentid":100, "lang": "ru",
-					 "cmd":"classbook_list","m":"m7334"})
-	ws.send(json_request)
-	response = json.loads(ws.recv())
-	print("Response: %s" % response)
-	must_be = json.loads(
-		"""{
+    """Testcase for classbook_list handler with exist localization
+     lang=ru and default parentid"""
+    json_request = json.dumps({"parentid": 100, "lang": "ru",
+                               "cmd": "classbook_list", "m": "m7334"})
+    ws.send(json_request)
+    response = json.loads(ws.recv())
+    print("Response: %s" % response)
+    must_be = json.loads(
+            """{
 			    'cmd': 'classbook_list',
 			    'data': [{
 			        'childs': 0,
@@ -107,4 +116,4 @@ def test_with_exist_lang_default_parentid():
 			    'm': 'm7334',
 			    'result': 'DONE'
 			}""".replace("'", "\""))
-	assert response == must_be
+    assert response == must_be
